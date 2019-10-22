60781538d19d362bfde2b3a604b53e30567b71b1
Blake Mason <bmason@lsst.org>
2019-10-22 15:11:44 -0700

    [F] Update utils and big to fetch antares data
    No implementation of data vis; just the fetch

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -11,23 +11,26 @@ export const AntaresClient = function async() {
     const locusIds = await axios.get(
       `https://antares.noao.edu/api/alerts/cone_search/?ra=${ra}&dec=${dec}&radius=${radius}`
     );
-    return locusIds.result[0];
+    return locusIds.data.result[0].locus_id;
   };
 
   Api.getLightCurve = async function(locusId) {
     const lightCurve = await axios.get(
       `https://antares.noao.edu/alerts/lightcurve-data/?locus_id=${locusId}`
     );
-    return lightCurve;
+
+    return lightCurve.data.result;
   };
 
   Api.getLightCurves = async function(ra, dec, radius) {
     const locusId = await this.getLocusId(ra, dec, radius);
+
     if (locusId === undefined || locusId.length === 0) {
       return {};
     }
-    const lightCurves = this.getLightCurve(locusId);
-    return lightCurves.result;
+
+    const lightCurves = await this.getLightCurve(locusId);
+    return lightCurves;
   };
 
   return Api;
